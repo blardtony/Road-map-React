527e039ab866ef666aa0e9a21d7cfa1482307081
Tony Blard <tonyblard55@gmail.com>
2023-10-25 14:37:42 +0200

    Create Counter component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <div className="card">
+                <h2>Counter</h2>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default Counter
