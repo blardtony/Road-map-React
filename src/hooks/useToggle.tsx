56863f45c9cedb967e08b27eda2f0d1584b88666
Tony Blard <tonyblard55@gmail.com>
2023-10-25 16:00:31 +0200

    Todo UseToggle

---
+++ src/hooks/useToggle.tsx
@@ -0,0 +1,10 @@
+import {useCallback, useMemo, useState} from "react";
+
+const useToggle = () => {
+
+  //const [status , setStatus] = useState<boolean>(false);
+  const [status, setStatus] = useState(false);
+  //TODO
+};
+
+export default useToggle;
