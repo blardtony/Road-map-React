e3e099217381ea6eff17259f2ce486969bd69e6f
Tony Blard <tonyblard55@gmail.com>
2023-10-25 14:50:01 +0200

    Add List component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
-import Counter from "./components/Counter.tsx";
-import Welcome from "./components/Welcome.tsx";
+import Counter from "./components/Counter/Counter.tsx";
+import Welcome from "./components/Welcome/Welcome.tsx";
+import List from "./components/List/List.tsx";
 
 function App() {
   const welcomeMessage: string = 'Hello World';
@@ -8,6 +9,7 @@ function App() {
     <>
       <Welcome message={welcomeMessage} />
       <Counter />
+      <List />
     </>
   )
 }
