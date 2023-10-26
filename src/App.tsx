4c4f86e0e22ad9b372d585c07f5bd593967a6c3c
Tony Blard <tonyblard55@gmail.com>
2023-10-26 14:50:09 +0200

    Router : Add RouterProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
-import Counter from "./components/Counter/Counter.tsx";
-import Welcome from "./components/Welcome/Welcome.tsx";
-import List from "./components/List/List.tsx";
-import Accordion from "./components/Accordion/Accordion.tsx";
+import {RouterProvider} from "react-router-dom";
+import {BrowserRouter} from "./components/BrowserRouter/BrowserRouter.tsx";
 
 function App() {
-  const welcomeMessage: string = 'Hello World';
   return (
     <>
-      <Welcome message={welcomeMessage} />
-      <Counter />
-      <List />
-      <Accordion />
+      <RouterProvider router={BrowserRouter}  />
     </>
   )
 }
