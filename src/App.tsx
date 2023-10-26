18ddc9e92d25b473c2c6f7ca325cd046c9cb655e
Tony Blard <tonyblard55@gmail.com>
2023-10-26 15:23:41 +0200

    Context : Add UserContext Provider in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import {RouterProvider} from "react-router-dom";
 import {BrowserRouter} from "./components/BrowserRouter/BrowserRouter.tsx";
+import UserContext, {defaultUserData} from "./contexts/UserContext/UserContext.tsx";
+import {useState} from "react";
 
 function App() {
+  const [userData, setUserData] = useState(defaultUserData);
   return (
     <>
-      <RouterProvider router={BrowserRouter}  />
+      <UserContext.Provider value={{userData, setUserData}}>
+        <RouterProvider router={BrowserRouter} />
+      </UserContext.Provider>
     </>
   )
 }
