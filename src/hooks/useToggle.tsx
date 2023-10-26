a6377a077eb534d833f61497c208c2c9b257d387
Tony Blard <tonyblard55@gmail.com>
2023-10-26 08:17:57 +0200

    Create custom hook

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,10 +1,20 @@
 import {useCallback, useMemo, useState} from "react";
 
+// Custom Hook
 const useToggle = () => {
+  const [status , setStatus] = useState<boolean>(false);
 
-  //const [status , setStatus] = useState<boolean>(false);
-  const [status, setStatus] = useState(false);
-  //TODO
+  const toggleStatus = useCallback(() => {
+    setStatus((previousStatus) => !previousStatus);
+  } , []);
+
+  return useMemo(
+    () => ({
+      status,
+      toggleStatus
+    }),
+    [status, toggleStatus]
+  );
 };
 
 export default useToggle;
