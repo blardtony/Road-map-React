16553c92ad8e7e3bb881e80e2723aa4b7b1d3a12
Tony Blard <tonyblard55@gmail.com>
2023-10-26 14:48:41 +0200

    Router : Create Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+  return(
+    <header>
+      <NavLink to="/" className={({isActive}) => {
+        return isActive ? "active" : "";
+      }}>
+        Home
+      </NavLink>
+      <NavLink to="/counter" className={({isActive}) => {
+        return isActive ? "active" : "";
+      }}>
+        Counter
+      </NavLink>
+      <NavLink to="/accordion" className={({isActive}) => {
+        return isActive ? "active" : "";
+      }}>
+        Accordion
+      </NavLink>
+      <NavLink to="/list" className={({isActive}) => {
+        return isActive ? "active" : "";
+      }}>
+        List
+      </NavLink>
+    </header>
+  )
+}
+
+export default Header;
