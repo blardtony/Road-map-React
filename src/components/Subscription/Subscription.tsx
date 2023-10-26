9db054607ff00d175a37db3b3b2304baf0160d64
Tony Blard <tonyblard55@gmail.com>
2023-10-26 15:48:36 +0200

    Tailwindcss : Test UI with tailwind

--- src/components/Subscription/Subscription.tsx
+++ src/components/Subscription/Subscription.tsx
@@ -3,27 +3,28 @@ import UserContext from "../../contexts/UserContext/UserContext";
 
 const Subscription: FC = () => {
   const { userData, setUserData } = useContext(UserContext);
+  const classInput = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6";
   return (
     <>
       <div>
         First Name :
-        <input id="firstName" name="firstName" value={userData.firstName} onChange={(e) => setUserData({ ...userData, firstName: e.target.value })} />
+        <input className={classInput} id="firstName" name="firstName" value={userData.firstName} onChange={(e) => setUserData({ ...userData, firstName: e.target.value })} />
       </div>
       <div>
         Last Name :
-        <input id="lastName" name="lastName" value={userData.lastName} onChange={(e) => setUserData({ ...userData, lastName: e.target.value })} />
+        <input className={classInput} id="lastName" name="lastName" value={userData.lastName} onChange={(e) => setUserData({ ...userData, lastName: e.target.value })} />
       </div>
       <div>
         Birth date :
-        <input id="birthDate" name="birthDate" value={userData.birthDate} onChange={(e) => setUserData({ ...userData, birthDate: e.target.value })} />
+        <input className={classInput} id="birthDate" name="birthDate" value={userData.birthDate} onChange={(e) => setUserData({ ...userData, birthDate: e.target.value })} />
       </div>
       <div>
         Email :
-        <input id="email" name="email" value={userData.email} onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
+        <input className={classInput} id="email" name="email" value={userData.email} onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
       </div>
       <div>
         Phone :
-        <input id="phoneNumber" name="phoneNumber" value={userData.phoneNumber} onChange={(e) => setUserData({ ...userData, phoneNumber: e.target.value })} />
+        <input className={classInput} id="phoneNumber" name="phoneNumber" value={userData.phoneNumber} onChange={(e) => setUserData({ ...userData, phoneNumber: e.target.value })} />
       </div>
     </>
   );
